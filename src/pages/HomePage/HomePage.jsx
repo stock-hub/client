2764605ce8458c9dff81449210cd6c4b4cdbfeba
Andrés García <playsharp59@gmail.com>
2022-10-22 18:29:10 +0200

    Added shop logo

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,10 @@
-
+import "./HomePage.css"
 
 const HomePage = () => {
     return (
         <>
-            <h3>Hello World</h3>
+            <h1>FERRETERIA LA 2</h1>
+            <img className="homeLogo" src="https://res.cloudinary.com/andresgarcia/image/upload/v1666456003/la2da/Page%20Assets/305967331_481477993986832_7342486477166241658_n_qcxsxt.jpg" alt="Shop logo" />
         </>
     )
 }
