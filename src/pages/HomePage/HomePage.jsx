8dc7ccf612305180336cb9dac64eb85cbd572159
Andrés García <playsharp59@gmail.com>
2023-02-25 17:38:58 +0100

    Remove images and change icon

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,7 +4,6 @@ const HomePage = () => {
     return (
         <>
             <h1>FERRETERIA LA 2</h1>
-            <img className="homeLogo" src="https://res.cloudinary.com/andresgarcia/image/upload/v1666456003/la2da/Page%20Assets/305967331_481477993986832_7342486477166241658_n_qcxsxt.jpg" alt="Shop logo" />
         </>
     )
 }
