945a4c104820001561a7b70a9a63421bd7d50bd6
Andrés García <playsharp59@gmail.com>
2023-03-01 19:30:32 +0100

    Set temporal homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,24 @@
-import "./HomePage.css"
+import styled from 'styled-components'
 
+const ComingSoonHeader = styled.h1`
+    font-weight: 700;
+    margin: 0;
+    border-bottom: 4px solid black;
+`
+
+const HomePageBg = styled.div`
+    background-color: #5c5c5c;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
 const HomePage = () => {
     return (
-        <>
-            <h1>FERRETERIA LA 2</h1>
-        </>
+        <HomePageBg>
+            <ComingSoonHeader>Coming soon!</ComingSoonHeader>
+        </HomePageBg>
     )
 }
 
