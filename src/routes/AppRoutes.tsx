c726f5aee6009b872328b45adcf3d30e25cad857
Andrés García <playsharp59@gmail.com>
2024-06-17 19:12:10 +0200

    feat: improve login check

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { DashboardPage } from '../pages/dashboard/DashboardPage/DashboardPage'
 import { AuthContext } from '../context/auth.context'
 import { LoginPage } from '../pages/dashboard/LoginPage/LoginPage'
@@ -9,22 +9,32 @@ import { ViewProductPage } from '../pages/dashboard/ViewProductPage/ViewProductP
 import { HomePage } from '../pages/HomePage/HomePage'
 import { PageNotFound } from '../pages/PageNotFound/PageNotFound'
 import { InvoicesPage } from '../pages/dashboard/InvoicesPage/InvoicesPage'
+import LoadingSpinner from '../components/LoadingSpinner/LoadingSpinner'
 
 export const AppRoutes: React.FC = () => {
-  const { isLoggedIn } = useContext(AuthContext)
+  const { isLoggedIn, isLoading } = useContext(AuthContext)
+
+  if (isLoading) {
+    return <LoadingSpinner />
+  }
 
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/dashboard/login' element={<LoginPage />} />
-      {isLoggedIn && (
+      {isLoggedIn ? (
         <>
           <Route path='/dashboard' element={<DashboardPage />} />
           <Route path='/dashboard/products' element={<ProductsPage />} />
           <Route path='/dashboard/products/new' element={<NewProductPage />} />
-          <Route path='/dashboard/products/:productId' element={<ViewProductPage />} />
+          <Route
+            path='/dashboard/products/:productId'
+            element={<ViewProductPage />}
+          />
           <Route path='/dashboard/invoices' element={<InvoicesPage />} />
         </>
+      ) : (
+        <Route path='*' element={<Navigate to='/dashboard/login' />} />
       )}
       <Route path='*' element={<PageNotFound />} />
     </Routes>
