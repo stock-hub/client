56d7283d42e1ce09fb5e228a0f5307d4ab96a946
Andrés García <playsharp59@gmail.com>
2024-07-20 02:38:16 +0200

    chore: enhance gallery grid display

---
+++ src/components/dashboard/ViewProductImgs/ViewProductImgs.styled.ts
@@ -0,0 +1,197 @@
+import styled from 'styled-components'
+
+export const Gallery = styled.div<{ imgCount: number }>`
+  display: grid;
+  grid-gap: 5px;
+  grid-template-columns: 10;
+  grid-template-rows: 5;
+  position: relative;
+  margin-bottom: 2rem;
+
+  & .gallery__img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  ${({ imgCount }) =>
+    imgCount === 1 &&
+    `
+
+& .gallery__item--1 {
+  grid-template-columns: 4fr;
+  grid-template-rows: 1fr;
+  grid-column: 2 / 3;
+  grid-row: 1 / 2;
+  margin: 0;
+  
+  & img {
+    border-radius: 10px;
+    width: 70%
+  }
+}
+
+& .loading-image {
+  grid-column: 1 / 9;
+  grid-row: 1 / 9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  & img {
+    width: 100px;
+    height: 100px;
+  }
+}
+`}
+
+  ${({ imgCount }) =>
+    imgCount === 2 &&
+    `
+
+& .gallery__item--1 {
+  grid-column: 1 / 5;
+  grid-row: 1 / 5;
+  margin: 0;
+  
+  & img {
+    border-radius: 10px 0 0 10px;
+  }
+}
+
+& .gallery__item--2 {
+  grid-column: 5 / 10;
+  grid-row: 1 / 5;
+  margin: 0;
+
+  & img {
+    border-radius: 0 10px 10px 0;
+  }
+}
+
+& .loading-image {
+  grid-column: 1 / 9;
+  grid-row: 1 / 9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  & img {
+    width: 100px;
+    height: 100px;
+  }
+}
+`}
+
+  ${({ imgCount }) =>
+    imgCount === 3 &&
+    `
+
+& .gallery__item--1 {
+  grid-column: 1 / 3;
+  grid-row: 1 / 3;
+  margin: 0;
+  
+  & img {
+    border-radius: 10px 0 0 10px;
+  }
+}
+
+& .gallery__item--2 {
+  grid-column: 4 / 6;
+  grid-row: 1 / 3;
+  margin: 0;
+}
+
+& .gallery__item--3 {
+  grid-column: 7 / 9;
+  grid-row: 1 / 3;
+  margin: 0;
+  
+  & img {
+    border-radius: 0 10px 10px 0;
+  }
+}
+
+& .loading-image {
+  grid-column: 1 / 9;
+  grid-row: 1 / 9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  & img {
+    width: 100px;
+    height: 100px;
+  }
+}
+`}
+
+  ${({ imgCount }) =>
+    imgCount > 3 &&
+    `
+
+& .gallery__item--1 {
+  grid-column: 1 / 5;
+  grid-row: 1 / 5;
+  margin: 0;
+  
+  & img {
+    border-radius: 10px 0 0 10px;
+  }
+}
+
+& .gallery__item--2 {
+  grid-column: 5 / 7;
+  grid-row: 1 / 3;
+  margin: 0;
+}
+
+& .gallery__item--3 {
+  grid-column: 7 / 9;
+  grid-row: 1 / 3;
+  margin: 0;
+  
+  & img {
+    border-radius: 0 10px 0 0;
+  }
+}
+
+& .gallery__item--4 {
+  grid-column: 5 / 7;
+  grid-row: 3 / 5;
+  margin: 0;
+}
+
+& .gallery__item--5 {
+  grid-column: 7 / 9;
+  grid-row: 3 / 5;
+  margin: 0;
+  
+  & img {
+    border-radius: 0 0 10px 0;
+  }
+}
+
+& .loading-image {
+  grid-column: 1 / 9;
+  grid-row: 1 / 9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  & img {
+    width: 100px;
+    height: 100px;
+  }
+}
+`}
+`
+
+export const Carousel = styled.div`
+  & img {
+    width: 100%;
+    height: 15rem;
+    object-fit: cover;
+  }
+`
--- src/components/dashboard/ViewProductImgs/ViewProductImgs.tsx
+++ src/components/dashboard/ViewProductImgs/ViewProductImgs.tsx
@@ -1,135 +1,63 @@
 import React from 'react'
-import styled from 'styled-components'
+import { Gallery, Carousel } from './ViewProductImgs.styled'
 
 type Props = {
   imgs: string[] | undefined
 }
 
-const Gallery = styled.div`
-  display: grid;
-  grid-template-columns: repeat(8, 1fr);
-  grid-template-rows: repeat(16, 1fr);
-  grid-gap: 5px;
-
-  & .gallery__img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-
-  & .gallery__item--1 {
-    grid-column-start: 1;
-    grid-column-end: 5;
-    grid-row-start: 1;
-    grid-row-end: 17;
-    margin: 0;
-
-    & img {
-      border-radius: 10px 0 0 10px;
-    }
-  }
-
-  & .gallery__item--2 {
-    grid-column-start: 5;
-    grid-column-end: 7;
-    grid-row-start: 1;
-    grid-row-end: 9;
-    margin: 0;
-  }
-
-  & .gallery__item--3 {
-    grid-column-start: 7;
-    grid-column-end: 9;
-    grid-row-start: 1;
-    grid-row-end: 9;
-    margin: 0;
-
-    & img {
-      border-radius: 0 10px 0 0;
-    }
-
-    & .gallery__item--4 {
-      grid-column-start: 5;
-      grid-column-end: 7;
-      grid-row-start: 9;
-      grid-row-end: 17;
-      margin: 0;
-    }
-
-    & .gallery__item--5 {
-      grid-column-start: 7;
-      grid-column-end: 9;
-      grid-row-start: 9;
-      grid-row-end: 17;
-      margin: 0;
-
-      & img {
-        border-radius: 0 0 10px 0;
-      }
-    }
-  }
-`
-
-const Carousel = styled.div`
-  & img {
-    width: 100%;
-    height: 15rem;
-    object-fit: cover;
-  }
-`
-
 export const ViewProductImgs: React.FC<Props> = ({ imgs }) => {
   const isMobile = window.matchMedia('(max-width: 480px)').matches
-  const loadingImage =
-    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ALoading_icon.gif&psig=AOvVaw1XQ5qtfInku40St5KZ_HAT&ust=1666533660116000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCOja1uj_8_oCFQAAAAAdAAAAABAE'
+  const loadingImage = 'https://res.cloudinary.com/andresgarcia/image/upload/v1721432239/stockhub/Loading_icon.gif'
+
+  const imgCount = imgs ? imgs.length : 0
 
   return isMobile ? (
-    <Carousel id='carouselExampleControls' className='carousel slide' data-bs-ride='carousel' data-interval='false'>
-      <div className='carousel-inner'>
-        <div className='carousel-item active'>
-          <img src={imgs === undefined ? loadingImage : imgs[0]} className='d-block w-100' alt='first_image' />
-        </div>
-        <div className='carousel-item'>
-          <img src={imgs === undefined ? loadingImage : imgs[1]} className='d-block w-100' alt='second_image' />
-        </div>
-        <div className='carousel-item'>
-          <img src={imgs === undefined ? loadingImage : imgs[2]} className='d-block w-100' alt='third_image' />
-        </div>
-        <div className='carousel-item'>
-          <img src={imgs === undefined ? loadingImage : imgs[3]} className='d-block w-100' alt='fourth_image' />
-        </div>
-        <div className='carousel-item'>
-          <img src={imgs === undefined ? loadingImage : imgs[4]} className='d-block w-100' alt='fifth_image' />
-        </div>
+    <Carousel id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel" data-interval="false">
+      <div className="carousel-inner">
+        {imgs === undefined ? (
+          <div className="carousel-item active">
+            <img src={loadingImage} className="d-block w-100" alt="loading_image" />
+          </div>
+        ) : (
+          imgs.map((img, idx) => (
+            <div className={`carousel-item ${idx === 0 ? 'active' : ''}`} key={idx}>
+              <img src={img} className="d-block w-100" alt={`image_${idx}`} />
+            </div>
+          ))
+        )}
       </div>
       <button
-        className='carousel-control-prev'
-        type='button'
-        data-bs-target='#carouselExampleControls'
-        data-bs-slide='prev'
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="prev"
       >
-        <span className='carousel-control-prev-icon' aria-hidden='true'></span>
-        <span className='visually-hidden'>Previous</span>
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
       </button>
       <button
-        className='carousel-control-next'
-        type='button'
-        data-bs-target='#carouselExampleControls'
-        data-bs-slide='next'
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="next"
       >
-        <span className='carousel-control-next-icon' aria-hidden='true'></span>
-        <span className='visually-hidden'>Next</span>
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
       </button>
     </Carousel>
   ) : (
-    <Gallery>
-      {imgs?.map((imgPath, idx) => {
-        return (
+    <Gallery imgCount={imgCount}>
+      {imgs === undefined ? (
+        <div className="loading-image">
+          <img src={loadingImage} alt="loading_image" />
+        </div>
+      ) : (
+        imgs.map((imgPath, idx) => (
           <figure className={`gallery__item gallery__item--${idx + 1}`} key={idx}>
-            <img src={imgPath} className='gallery__img' alt={`gallery_image_${idx + 1}`} />
+            <img src={imgPath} className="gallery__img" alt={`gallery_image_${idx + 1}`} />
           </figure>
-        )
-      })}
+        ))
+      )}
     </Gallery>
   )
 }
